fa2242d17d9af06638e4d4613c7c0da594880f44
Yahya Efe Kuruçay <109385388+efekurucay@users.noreply.github.com>
2024-02-29 19:48:04 +0300

    update

--- CSE102T_Assignments/Assignment01/src/Assignment01_20220808005.java
+++ CSE102T_Assignments/Assignment01/src/Assignment01_20220808005.java
@@ -1,3 +1,20 @@
+
+/**---------------------------------------------------
+
+ * Akdeniz University CSE102T Assignments
+
+ * @author: Yahya Efe Kuruçay
+
+ * @since: 29.02.2024
+
+ * Description: Assignment01
+
+ * Score: ?
+
+ * Website: https://efekurucay.com
+
+*---------------------------------------------------*/
+
 public class Assignment01_20220808005 {
     public static void main(String[] args) {
         
